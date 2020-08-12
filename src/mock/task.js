a2787980c231c3f54e1ef5f4d00ced90991cee2c
Alena Shashko <L.Shashko@mail.ru>
2020-08-12 23:57:55 +0300

    generate task date and repeating

--- src/mock/task.js
+++ src/mock/task.js
@@ -12,8 +12,51 @@ const generateDescription = () => {
   return descriptions[randomIndex];
 };
 
+const generateDate = () => {
+  const isDate = Boolean(getRandomInteger(0, 1));
+
+  if (!isDate) {
+    return null;
+  }
+
+  const maxDaysGap = 7; // не должно быть прописными буквами?
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+  const currentDate = new Date();
+  console.log(new Date()); // ?
+
+  currentDate.setHours(23, 59, 59, 999);
+  currentDate.setDate(currentDate.getDate() + daysGap);
+
+  return new Date(currentDate);
+};
+
+const generateRepeating = () => {
+  return {
+    mo: false,
+    tu: false,
+    we: Boolean(getRandomInteger(0, 1)),
+    th: false,
+    fr: Boolean(getRandomInteger(0, 1)),
+    sa: false,
+    su: false
+  };
+};
+
 const generateTask = () => {
+  const dueDate = generateDate();
+  const repeating = (dueDate === null) ? generateRepeating()
+    : {
+      mo: false,
+      tu: false,
+      we: Boolean(getRandomInteger(0, 1)),
+      th: false,
+      fr: Boolean(getRandomInteger(0, 1)),
+      sa: false,
+      su: false
+    };
   return {
     description: generateDescription(),
+    DueDate,
+    repeating
   };
 };
