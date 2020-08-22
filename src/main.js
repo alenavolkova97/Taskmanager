0a03b13712629f6c30f6b9e408e316f4b30f49fd
Alena Shashko <L.Shashko@mail.ru>
2020-08-22 08:44:01 +0300

    rewrite board, sorting, button and tasklist to classes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import SiteMenuView from './view/site-menu.js';
 import {createFilterTemplate} from './view/filter.js';
-import {createBoardTemplate} from './view/board.js';
-import {createSortingTemplate} from './view/sorting.js';
+import BoardView from './view/board.js';
+import SortingView from './view/sorting.js';
+import TaskListView from './view/task-list.js';
 import {createTaskEditTemplate} from './view/task-edit.js';
 import {createTaskTemplate} from './view/task.js';
-import {createLoadMoreButtonTemplate} from './view/load-more-button.js';
+import LoadMoreButtonView from './view/load-more-button.js';
 import {generateTask} from './mock/task.js';
 import {generateFilter} from "./mock/filter.js";
 import {renderTemplate, renderElement, RenderPosition} from "./utils.js";
@@ -20,34 +21,37 @@ const filters = generateFilter(tasks);
 
 renderElement(headerElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND);
 renderTemplate(mainElement, createFilterTemplate(filters), `beforeend`);
-renderTemplate(mainElement, createBoardTemplate(), `beforeend`);
 
-const boardElement = mainElement.querySelector(`.board`);
-const boardTasksElement = boardElement.querySelector(`.board__tasks`);
+const boardComponent = new BoardView();
+const TaskListComponent = new TaskListView();
 
-renderTemplate(boardElement, createSortingTemplate(), `afterbegin`);
-renderTemplate(boardTasksElement, createTaskEditTemplate(tasks[0]), `beforeend`);
+renderElement(mainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
+renderElement(boardComponent.getElement(), new SortingView().getElement(), RenderPosition.BEFOREEND);
+renderElement(boardComponent.getElement(), TaskListComponent.getElement(), RenderPosition.BEFOREEND);
+
+renderTemplate(TaskListComponent.getElement(), createTaskEditTemplate(tasks[0]), `beforeend`);
 
 for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
-  renderTemplate(boardTasksElement, createTaskTemplate(tasks[i]), `beforeend`);
+  renderTemplate(TaskListComponent.getElement(), createTaskTemplate(tasks[i]), `beforeend`);
 }
 
 if (tasks.length > TASK_COUNT_PER_STEP) {
   let renderedTaskCount = TASK_COUNT_PER_STEP;
 
-  renderTemplate(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+  const LoadMoreButtonComponent = new LoadMoreButtonView();
 
-  const loadMoreButton = boardElement.querySelector(`.load-more`);
+  renderElement(boardComponent.getElement(), LoadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
 
-  loadMoreButton.addEventListener(`click`, () => {
+  LoadMoreButtonComponent.getElement().addEventListener(`click`, () => {
     tasks
       .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-      .forEach((task) => renderTemplate(boardTasksElement, createTaskTemplate(task), `beforeend`));
+      .forEach((task) => renderTemplate(TaskListComponent.getElement(), createTaskTemplate(task), `beforeend`));
 
     renderedTaskCount += TASK_COUNT_PER_STEP;
 
     if (renderedTaskCount >= tasks.length) {
-      loadMoreButton.remove();
+      LoadMoreButtonComponent.getElement().remove(); // delete Dom-element
+      LoadMoreButtonComponent.removeElement(); // set _element as null
     }
   });
 }
--- src/view/sorting.js
+++ src/view/sorting.js
@@ -1,9 +1,33 @@
-export const createSortingTemplate = () => {
+import {createElement} from '../utils.js';
+
+const createSortingTemplate = () => {
   return (
     `<div class="board__filter-list">
-    <a href="#" class="board__filter">SORT BY DEFAULT</a>
-    <a href="#" class="board__filter">SORT BY DATE up</a>
-    <a href="#" class="board__filter">SORT BY DATE down</a>
+      <a href="#" class="board__filter">SORT BY DEFAULT</a>
+      <a href="#" class="board__filter">SORT BY DATE up</a>
+      <a href="#" class="board__filter">SORT BY DATE down</a>
     </div>`
   );
 };
+
+export default class Sorting {
+  constructor() {
+    this._element = null;
+  }
+
+  _getTemplate() {
+    return createSortingTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this._getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() { // ?
+    this._element = null;
+  }
+}
